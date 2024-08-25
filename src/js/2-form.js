ba427a40cc717d0fee92e3f8c70a06486f221bc4
Roman <romka22830@gmail.com>
2024-08-25 15:18:13 +0300

    update

---
+++ src/js/2-form.js
@@ -0,0 +1,37 @@
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  formData = JSON.parse(savedData);
+  emailInput.value = formData.email;
+  messageInput.value = formData.message;
+}
+
+form.addEventListener('input', evt => {
+  formData[evt.target.name] = evt.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: '', message: '' };
+  form.reset();
+});
